7de69c2763621742bbd31370561109c0d8426a4d
Pedro Torres <kitschest@Pedros-MacBook-Air.local>
2023-04-26 13:23:25 +0200

    collisions added

--- js/index.js
+++ js/index.js
@@ -1,144 +1,25 @@
-//Coordenadas
-
-/* tiles = [
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0},
-    {x: 0, y: 0}, {x: 0, y: 0}, {x: 0, y: 0}, ] */
-
-
-
-
-const x1 = 0;
-const x2 = 20;
-const x3 = 40
-const x4 = 60
-const x5 = 80
-const x6 = 100
-const x7 = 120
-const x8 = 140
-const x9 = 160
-const x10 = 180
-const x11 = 200
-const x12 = 220
-const x13 = 240
-const x14 = 260
-const x15 = 280
-const x16 = 300
-const x17 = 320
-const x18 = 340
-const x19 = 360
-const x20 = 380
-const x21 = 400
-const x22 = 420
-const x23 = 440
-const x24 = 460
-const x25 = 480
-const x26 = 500
-const x27 = 520
-const x28 = 540
-const x29 = 560
-const x30 = 580
-const x31 = 600
-const x32 = 620
-const x33 = 640
-const x34 = 660
-const x35 = 680
-const x36 = 700
-const x37 = 720
-const x38 = 740
-const x39 = 760
-const x40 = 780
-
-
-
-const y1 = 0;
-const y2 = 20;
-const y3 = 40
-const y4 = 60
-const y5 = 80
-const y6 = 100
-const y7 = 120
-const y8 = 140
-const y9 = 160
-const y10 = 180
-const y11 = 200
-const y12 = 220
-const y13 = 240
-const y14 = 260
-const y15 = 280
-const y16 = 300
-const y17 = 320
-const y18 = 340
-const y19 = 360
-const y20 = 380
-const y21 = 400
-const y22 = 420
-const y23 = 440
-const y24 = 460
-const y25 = 480
-const y26 = 500
-const y27 = 520
-const y28 = 540
-const y29 = 560
-const y30 = 580
-
-/* class Game {
-    constructor {
-        this.canvasB = document.getElementById("canvasBack");
-        this.ctxB = canvasB.getContext("2d");
-        this.canvasF = document.getElementById("canvasFront");
-        this.ctxF = canvasF.getContext("2d");
-
-    }
-} */
+// start
 
 let canvasB = document.getElementById("canvasBack");
 let ctxB = canvasB.getContext("2d");
 let canvasF = document.getElementById("canvasFront");
 let ctxF = canvasF.getContext("2d");
 
-
-//Player animation images
 let playerSprite = document.createElement("img");
-playerSprite.src = "images/BILLY_BIT.png";
-
-// let playerUp1 = document.createElement("img");
-// playerUp1.src = "images/player/up1.png";
-// let playerUp2 = document.createElement("img");
-// playerUp2.src = "images/player/up2.png";
-
-// let playerRight1 = document.createElement("img");
-// playerRight1.src = "images/player/right1.png";
-// let playerRight2 = document.createElement("img");
-// playerRight2.src = "images/player/right2.png";
-
-// let playerDown1 = document.createElement("img");
-// playerDown1.src = "images/player/down1.png";
-// let playerDown2 = document.createElement("img");
-// playerDown2.src = "images/player/down2.png";
-
-// let playerLeft1 = document.createElement("img");
-// playerLeft1.src = "images/player/left1.png";
-// let playerLeft2 = document.createElement("img");
-// playerLeft2.src = "images/player/left2.png";
-
-
-
+playerSprite.src = "./images/BILLY_BIT.png";
 
 let background = document.createElement("img");
-background.src = "images/PhantomRunner1.2.png"
+background.src = "./images/Sin_titulo.png" //"./images/main-background.png"
 
 background.onload = () => {
     ctxB.drawImage(background,0,0,800,600)
 }
 
-let gradient = document.createElement("img");
-gradient.src = "images/LAYER2.png"
+// let gradient = document.createElement("img");
+// gradient.src = "images/LAYER2.png"
+
+let ghostSprite = document.createElement("img");
+ghostSprite.src = "./images/ghost1.png";
 
 
 ctxF.fillStyle = "black";
@@ -151,8 +32,8 @@ let direction = "standingDown";
 
 const player = {
     
-    x: 400,
-    y: 300,
+    x: 402,
+    y: 292,
     arcX: 410,
     arcY: 310,
     gradX: -393  ,  //355,
@@ -178,13 +59,24 @@ const player = {
     },
 
     recalculatePosition: function(incX, incY) {
-        this.x += incX;
-        this.arcX += incX;
-        this.gradX += incX;
-        this.y += incY;
-        this.arcY += incY;
-        this.gradY += incY;
+        let newX = this.x + incX;
+        let newY = this.y + incY;
+    
+        if (canMoveTo(newX, newY, 18, 23)) {
+            if (newX >= 0 && newX <= canvasB.width - 18) { // 18 is the player's width
+                this.x = newX;
+                this.arcX += incX;
+                this.gradX += incX;
+            }
+    
+            if (newY >= 0 && newY <= canvasB.height - 23) { // 23 is the player's height
+                this.y = newY;
+                this.arcY += incY;
+                this.gradY += incY;
+            }
+        }
     },
+    
 
     print: function() {
         if (direction == "standingUp") {
@@ -218,55 +110,26 @@ const player = {
     }
 }
 
-
-/* class Obstacle {
-    constructor (x,y)
-} */
-
-
 const update = function() {
-    //limpiar
-    ctxF.restore()
-    ctxB.clearRect(0,0,800,600);
-    ctxF.clearRect(0,0,800,600);
+    // Clear
+    ctxF.restore();
+    ctxB.clearRect(0, 0, 800, 600);
+    ctxF.clearRect(0, 0, 800, 600);
 
-    //recalcular (incorporar obstáculos)
+    ctxB.drawImage(background, 0, 0, 800, 600);
 
-
-    //redibujar
-    // ctxB.fillStyle = "yellow";
-    // ctxB.fillRect(0,0,800,600);
-    ctxB.drawImage(background,0,0,800,600);
-
-    ctxB.fillStyle = "green";
-    ctxB.fillRect(x16,y16,20,20);
-    ctxB.fillRect(x17,y16,20,20);
 
 
     player.print();
+    ghost.print(); // Add this line to draw the Ghost
 
-/*     ctxF.fillStyle = "rgba(0, 0, 0, 0.7)";
-    ctxF.fillRect(0,0,800,600);
+    ctxF.drawImage(gradient, player.gradX, player.gradY, 1600, 1200)
 
-    ctxF.save()
-
-    ctxF.beginPath()
-    ctxF.arc(player.arcX,player.arcY,50,0,2*Math.PI);
-    ctxF.stroke();
-    ctxF.clip();
-    ctxF.clearRect(0,0,800,600); */
-    ctxF.drawImage(gradient,player.gradX,player.gradY,1600,1200)
-
-    //ctxF.drawImage(gradient,player.gradX,player.gradY,110,110)
-}
-
-let intervalId = setInterval(update,60);
+    checkGhostCollision(); // Add this line to check for collisions
+};
 
 
-
-
-
-
+    let intervalId = setInterval(update,60);
 
 let timeoutIdUp
 let timeoutIdRight
@@ -328,3 +191,105 @@ document.body.addEventListener("keyup", (e)=>{
         timeoutIdLeft = setTimeout(() => {direction = "standingLeft"}, 400)
     }
 })
+
+
+// new changes //
+
+
+const obstacles = [
+    // obstacle data here:
+
+    {x: 0, y: 0, w: 220, h: 40}, 
+    {x: 220, y: 0, w: 40, h: 100}, 
+    {x: 260, y: 0, w: 540, h: 40},
+    {x: 220, y: 120, w: 40, h: 160},
+    {x: 260, y: 160, w: 160, h: 60},
+    {x: 440, y: 160, w: 160, h: 60}, 
+    {x: 560, y: 220, w: 40, h: 60}, 
+    {x: 600, y: 180, w: 100, h: 60},
+    {x: 720, y: 180, w: 80, h: 60}, 
+    {x: 0, y: 380, w: 80, h: 60}, 
+    {x: 100, y: 380, w: 160, h: 60},
+    {x: 220, y: 300, w: 40, h: 80}, 
+    {x: 240, y: 400, w: 80, h: 60}, 
+    {x: 340, y: 400, w: 160, h: 60}, 
+    {x: 520, y: 400, w: 80, h: 60},
+    {x: 560, y: 300, w: 40, h: 100},
+    {x: 580, y: 420, w: 180, h: 60}, 
+    {x: 780, y: 420, w: 20, h: 60},
+    {x: 400, y: 460, w: 40, h: 60}, 
+    {x: 400, y: 540, w: 40, h: 60},    
+  ];
+
+  function isColliding(rect1, rect2) {
+    return rect1.x < rect2.x + rect2.w &&
+           rect1.x + rect1.w > rect2.x &&
+           rect1.y < rect2.y + rect2.h &&
+           rect1.y + rect1.h > rect2.y;
+  }
+
+  function canMoveTo(newX, newY, playerWidth, playerHeight) {
+    const playerRect = {
+      x: newX,
+      y: newY,
+      w: playerWidth,
+      h: playerHeight
+    };
+  
+    for (const obstacle of obstacles) {
+      if (isColliding(playerRect, obstacle)) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+
+  function movePlayer(newX, newY) {
+    if (canMoveTo(newX, newY, playerWidth, playerHeight)) {
+      playerX = newX;
+      playerY = newY;
+      draw();
+    }
+  }
+
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'black';
+    for (const obstacle of obstacles) {
+      ctx.fillRect(obstacle.x, obstacle.y, obstacle.w, obstacle.h);
+    }
+
+    ctx.drawImage(playerImg, playerX, playerY, playerWidth, playerHeight);
+
+
+draw();
+
+movePlayer(playerX + 5, playerY);
+
+}
+
+// GHOST
+
+const ghost = {
+    x: 200,
+    y: 200,
+    width: 18,
+    height: 23,
+
+    print: function() {
+        ctxB.drawImage(ghostSprite, this.x, this.y, this.width, this.height);
+    }
+};
+
+function checkGhostCollision() {
+    if (isColliding(player, ghost)) {
+        player.x = 400;
+        player.y = 300;
+        player.arcX = 410;
+        player.arcY = 310;
+        player.gradX = -393;
+        player.gradY = -290;
+    }
+}
